32c8437b9d057aa75242148c9ff747f38ef2d0fe
krishna21august <krishnakant.singh.min12@gmail.com>
2019-01-06 17:27:20 +0530

    ingredients complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
-import React from "react";
-import Aux from "../../hoc/Aux";
-import classes from "./Layout.css";
-import Burger from "../Burger/Burger";
-const layout = props => (
-  <Aux>
-    <Burger />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const layout = ( props ) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
 );
 
 export default layout;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
-  render() {
-    return (
-      <Aux>
-        <div>Burger</div>
-        <div>Burger Controls</div>
-      </Aux>
-    );
-  }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
 }
 
 export default BurgerBuilder;

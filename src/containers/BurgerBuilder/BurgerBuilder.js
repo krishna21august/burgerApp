1e2207c5b3743b1553259225e732795d757293b2
safe <krishna.s@lucideustech.com>
2019-01-09 18:25:38 +0530

    burger with pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {...}
-    // }
     state = {
         ingredients: {
             salad: 0,
@@ -17,12 +14,12 @@ class BurgerBuilder extends Component {
         }
     }
 
-    render () {
+    render() {
         return (
-            <Aux>
+            <>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
-            </Aux>
+                <BuildControls />
+            </>
         );
     }
 }

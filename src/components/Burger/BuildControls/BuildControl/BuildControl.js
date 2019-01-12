88c6bfa33524e097493ce6ff878bccb771c43eaa
safe <krishna.s@lucideustech.com>
2019-01-12 19:53:10 +0530

    css changes

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,14 +1,18 @@
 import React from 'react';
+
 import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <label className={classes.Label}>{props.label}</label>
-        <button onClick={props.remove} className={classes.Less} disabled={props.disabled}>Less</button>
-        <button onClick={props.add} className={classes.More}>More</button>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
     </div>
-
 );
-
 
 export default buildControl;

6c37421c5ff47ecea9b361d2d3450ee99a89bf17
safe <krishna.s@lucideustech.com>
2019-01-23 11:10:06 +0530

    add contact data spinner and pass on data from top component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from '../Checkout/ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 4,
-            bacon: 4,
-            meat: 4,
-            cheese: 4
-        }
+        ingredients: '',
+        price: 0
     }
 
     checkoutCancelledHandler = () => {
@@ -19,15 +17,18 @@ class Checkout extends Component {
         this.props.history.replace('/checkout/contact-data')
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
-
+        let price = 0;
         for (let param of query.entries()) {
+            if (param[0] === 'price') {
+                price = param[1];
+            }
             ingredients[param[0]] = +param[1];
         }
 
-        this.setState({ ingredients: ingredients })
+        this.setState({ ingredients: ingredients, totalPrice: price })
 
     }
 
@@ -37,6 +38,7 @@ class Checkout extends Component {
                 <CheckoutSummary ingredients={this.state.ingredients} checkoutContinued={this.checkoutContinuedHandler}
                     checkoutCancelled={this.checkoutCancelledHandler}
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={() => (<ContactData ingredients={this.state.ingredients} price={this.state.price} />)} />
             </div>
         );
 
